efd12c3e254ec5ba5a6a346643db263c0e962282
Ronalde Gonçalves <57369117+Ronalde-web@users.noreply.github.com>
2021-10-17 00:39:10 -0300

    Add files via upload

---
+++ Escolar.py
@@ -0,0 +1,10 @@
+nota_1 = float(input('digite a primeira nota: '))
+nota_2 = float(input('digite a segunda nota: '))
+nota_3 = float(input('digite a 3° nota: '))
+media = (nota_1 + nota_2 + nota_3)/3
+if media >= 7:
+    print("passou por media! ", media)
+else:
+    print("reprovou!", media)
+print("BOM DIA!")
+
